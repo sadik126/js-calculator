11317269dd4adf9c47e43cb4c6d3a6b95090a0c0
sadik126 <sadikhimel04@gmail.com>
2022-02-12 20:30:50 +0600

    m

--- calculator.js
+++ calculator.js
@@ -1,1 +1,9 @@
 let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button')
+for (const item of buttons) {
+    item.addEventListener('click',(e)=>{
+    //    let clickbtn = document.getElementById('button');
+       let buttontext = e.target.innertext;
+       console.log(buttontext);
+    })
+}
