62f06a30b150d91bf7872fec654d492bb9eb42eb
sadik126 <sadikhimel04@gmail.com>
2022-02-12 21:18:29 +0600

    m

--- calculator.js
+++ calculator.js
@@ -1,9 +1,34 @@
 let screen = document.getElementById('screen');
 buttons = document.querySelectorAll('button')
-for (const item of buttons) {
-    item.addEventListener('click',(e)=>{
-    //    let clickbtn = document.getElementById('button');
-       let buttontext = e.target.innertext;
+let screenvalue = '';
+
+for (let item of buttons) {
+    item.addEventListener('click',function(e){
+    //    let clickbtn = document.getElementsByClassName('button');
+       let buttontext = e.target.innerText;
        console.log(buttontext);
+
+       if(buttontext == 'X'){
+           buttontext = '*';
+           screenvalue += buttontext;
+           screen.value = screenvalue;
+       }
+       else if(buttontext == '='){
+        screen.value = eval(screenvalue);
+        
+       }
+       else if(buttontext == 'C'){
+           screenvalue = '';
+           screen.value = screenvalue;
+           
+       }
+       else{
+        screenvalue += buttontext;
+        screen.value = screenvalue;
+       }
     })
 }
+// document.getElementsByClassName('button').addEventListener('click',function(){
+//    console.log('hello') 
+// })
+
